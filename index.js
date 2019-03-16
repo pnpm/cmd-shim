4e935fd80744c52a91936f6167dda38d38c67ede
Tatsunori Uchino <tats.u@live.jp>
2019-03-16 18:59:26 +0900

    refactor: move some processes to new functions
    - Search script runtime
    - Generate a shim script

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ const DEFAULT_OPTIONS = {
   createPwshFile: true,
   createCmdFile: isWindows()
 }
+const extensionToProgramMap = new Map([
+  ['.js', 'node'],
+  ['.cmd', 'cmd'],
+  ['.bat', 'cmd'],
+  ['.ps1', 'pwsh'], // not 'powershell'
+  ['.sh', 'sh']
+])
 
 /**
  * Try to create shims.
@@ -72,88 +79,140 @@ function rm (path) {
 }
 
 function cmdShim_ (src, to, opts) {
-  return Promise.all([
-    rm(to),
-    rm(`${to}.ps1`),
-    opts.createCmdFile && rm(`${to}.cmd`)
-  ])
-    .then(() => writeShim(src, to, opts))
+  opts = Object.assign({}, DEFAULT_OPTIONS, opts)
+  return searchScriptRuntime(src).then((srcRuntimeInfo) => {
+    // Always tries to create all types of shims by calling `writeAllShims` as of now.
+    // Append your code here to change the behavior in response to `srcRuntimeInfo`.
+
+    // Create 3 shims for (Ba)sh in Cygwin / MSYS, no extension) & CMD (.cmd) & PowerShell (.ps1)
+    return writeAllShims(src, to, srcRuntimeInfo, opts)
+  })
 }
 
-function writeShim (src, to, opts) {
+function writeAllShims (src, to, srcRuntimeInfo, opts) {
   opts = Object.assign({}, DEFAULT_OPTIONS, opts)
-  const defaultArgs = opts.preserveSymlinks ? '--preserve-symlinks' : ''
-  // make a cmd file and a sh script
-  // First, check if the bin is a #! of some sort.
-  // If not, then assume it's something that'll be compiled, or some other
-  // sort of script, and just call it directly.
-  return mkdir(path.dirname(to))
-    .then(() => {
-      return fs.readFile(src, 'utf8')
-        .then(data => {
-          const firstLine = data.trim().split(/\r*\n/)[0]
-          const shebang = firstLine.match(shebangExpr)
-          if (!shebang) return writeShim_(src, to, Object.assign({}, opts, {args: defaultArgs}))
-          const prog = shebang[1]
-          const args = (shebang[2] && ((defaultArgs && (shebang[2] + ' ' + defaultArgs)) || shebang[2])) || defaultArgs
-          return writeShim_(src, to, Object.assign({}, opts, {prog, args}))
+  return Promise.all([
+    [generateShShim, '', true],
+    [generateCmdShim, '.cmd', opts.createCmdFile],
+    [generatePwshShim, '.ps1', opts.createPwshFile]
+  ].map(([generateShimScript, extension, shouldCreate]) => shouldCreate && writeShim(src, to + extension, srcRuntimeInfo, generateShimScript, opts)))
+}
+
+function writeShimPre (target) {
+  return rm(target).then(() => mkdir(path.dirname(target)))
+}
+
+function writeShimPost (target) {
+  // Only chmoding shims as of now.
+  // Some other processes may be appended.
+  return chmodShim(target)
+}
+
+function searchScriptRuntime (target) {
+  return fs
+    .readFile(target, 'utf8')
+    .then(data => {
+      // First, check if the bin is a #! of some sort.
+      const firstLine = data.trim().split(/\r*\n/)[0]
+      const shebang = firstLine.match(shebangExpr)
+      if (!shebang) {
+        // If not, infer script type from its extension.
+        // If the inferrence fails, it's something that'll be compiled, or some other
+        // sort of script, and just call it directly.
+        const targetExtension = path.extname(target).toLowerCase()
+        return Promise.resolve({
+          // undefined if extension is unknown but it's converted to null.
+          program: extensionToProgramMap.get(targetExtension) || null,
+          additionalArgs: ''
         })
-        .catch(() => writeShim_(src, to, Object.assign({}, opts, {args: defaultArgs})))
+      }
+      return Promise.resolve({
+        program: shebang[1],
+        additionalArgs: shebang[2]
+      })
     })
 }
 
-function writeShim_ (src, to, opts) {
-  opts = Object.assign({}, DEFAULT_OPTIONS, opts)
+function writeShim (src, to, srcRuntimeInfo, generateShimScript, opts) {
+  const defaultArgs = opts.preserveSymlinks ? '--preserve-symlinks' : ''
+  // `Array.prototype.flter` removes ''.
+  // ['--foo', '--bar'].join(' ') and [].join(' ') returns '--foo --bar' and '' respectively.
+  const args = [srcRuntimeInfo.additionalArgs, defaultArgs].filter(arg => arg).join(' ')
+  opts = Object.assign({}, opts, {
+    prog: srcRuntimeInfo.program,
+    args: args
+  })
+
+  return writeShimPre(to)
+    .then(() => fs.writeFile(to, generateShimScript(src, to, opts), 'utf8'))
+    .then(() => writeShimPost(to))
+}
+
+function generateCmdShim (src, to, opts) {
   let shTarget = path.relative(path.dirname(to), src)
   let target = shTarget.split('/').join('\\')
   let longProg
   let prog = opts.prog
-  let shProg = prog && prog.split('\\').join('/')
-  let shLongProg
-  let pwshProg = shProg && `"${shProg}$exe"`
-  let pwshLongProg
   shTarget = shTarget.split('\\').join('/')
   let args = opts.args || ''
   let {
-    win32: nodePath,
-    posix: shNodePath
+    win32: nodePath
   } = normalizePathEnvVar(opts.nodePath)
   if (!prog) {
     prog = `"%~dp0\\${target}"`
-    shProg = `"$basedir/${shTarget}"`
-    pwshProg = shProg
     args = ''
     target = ''
     shTarget = ''
   } else {
     longProg = `"%~dp0\\${prog}.exe"`
-    shLongProg = '"$basedir/' + prog + '"'
-    pwshLongProg = `"$basedir/${prog}$exe"`
     target = `"%~dp0\\${target}"`
     shTarget = `"$basedir/${shTarget}"`
   }
 
-  let cmd
-  if (opts.createCmdFile) {
-    // @IF EXIST "%~dp0\node.exe" (
-    //   "%~dp0\node.exe" "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
-    // ) ELSE (
-    //   SETLOCAL
-    //   SET PATHEXT=%PATHEXT:;.JS;=;%
-    //   node "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
-    // )
-    cmd = nodePath ? `@SET NODE_PATH=${nodePath}\r\n` : ''
-    if (longProg) {
-      cmd += '@IF EXIST ' + longProg + ' (\r\n' +
-        '  ' + longProg + ' ' + args + ' ' + target + ' %*\r\n' +
-        ') ELSE (\r\n' +
-        '  @SETLOCAL\r\n' +
-        '  @SET PATHEXT=%PATHEXT:;.JS;=;%\r\n' +
-        '  ' + prog + ' ' + args + ' ' + target + ' %*\r\n' +
-        ')'
-    } else {
-      cmd += `@${prog} ${args} ${target} %*\r\n`
-    }
+  // @IF EXIST "%~dp0\node.exe" (
+  //   "%~dp0\node.exe" "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
+  // ) ELSE (
+  //   SETLOCAL
+  //   SET PATHEXT=%PATHEXT:;.JS;=;%
+  //   node "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
+  // )
+  let cmd = nodePath ? `@SET NODE_PATH=${nodePath}\r\n` : ''
+  if (longProg) {
+    cmd += '@IF EXIST ' + longProg + ' (\r\n' +
+      '  ' + longProg + ' ' + args + ' ' + target + ' %*\r\n' +
+      ') ELSE (\r\n' +
+      '  @SETLOCAL\r\n' +
+      '  @SET PATHEXT=%PATHEXT:;.JS;=;%\r\n' +
+      '  ' + prog + ' ' + args + ' ' + target + ' %*\r\n' +
+      ')'
+  } else {
+    cmd += `@${prog} ${args} ${target} %*\r\n`
+  }
+
+  return cmd
+}
+
+function generateShShim (src, to, opts) {
+  let shTarget = path.relative(path.dirname(to), src)
+  let target = shTarget.split('/').join('\\')
+  let prog = opts.prog
+  let shProg = prog && prog.split('\\').join('/')
+  let shLongProg
+  shTarget = shTarget.split('\\').join('/')
+  let args = opts.args || ''
+  let {
+    posix: shNodePath
+  } = normalizePathEnvVar(opts.nodePath)
+  if (!prog) {
+    prog = `"%~dp0\\${target}"`
+    shProg = `"$basedir/${shTarget}"`
+    args = ''
+    target = ''
+    shTarget = ''
+  } else {
+    shLongProg = '"$basedir/' + prog + '"'
+    target = `"%~dp0\\${target}"`
+    shTarget = `"$basedir/${shTarget}"`
   }
 
   // #!/bin/sh
@@ -195,6 +254,37 @@ function writeShim_ (src, to, opts) {
   } else {
     sh = sh + env + shProg + ' ' + args + ' ' + shTarget + ' "$@"\n' +
       'exit $?\n'
+  }
+
+  return sh
+}
+
+function generatePwshShim (src, to, opts) {
+  let shTarget = path.relative(path.dirname(to), src)
+  let target = shTarget.split('/').join('\\')
+  let prog = opts.prog
+  let shProg = prog && prog.split('\\').join('/')
+  let shLongProg
+  let pwshProg = shProg && `"${shProg}$exe"`
+  let pwshLongProg
+  shTarget = shTarget.split('\\').join('/')
+  let args = opts.args || ''
+  let {
+    win32: nodePath,
+    posix: shNodePath
+  } = normalizePathEnvVar(opts.nodePath)
+  if (!prog) {
+    prog = `"%~dp0\\${target}"`
+    shProg = `"$basedir/${shTarget}"`
+    pwshProg = shProg
+    args = ''
+    target = ''
+    shTarget = ''
+  } else {
+    shLongProg = '"$basedir/' + prog + '"'
+    pwshLongProg = `"$basedir/${prog}$exe"`
+    target = `"%~dp0\\${target}"`
+    shTarget = `"$basedir/${shTarget}"`
   }
 
   // #!/usr/bin/env pwsh
@@ -252,20 +342,11 @@ function writeShim_ (src, to, opts) {
       'exit $LASTEXITCODE\n'
   }
 
-  return Promise.all([
-    opts.createCmdFile && fs.writeFile(to + '.cmd', cmd, 'utf8'),
-    opts.createPwshFile && fs.writeFile(`${to}.ps1`, pwsh, 'utf8'),
-    fs.writeFile(to, sh, 'utf8')
-  ])
-    .then(() => chmodShim(to, opts))
+  return pwsh
 }
 
-function chmodShim (to, {createCmdFile, createPwshFile}) {
-  return Promise.all([
-    fs.chmod(to, 0o755),
-    createPwshFile && fs.chmod(`${to}.ps1`, 0o755),
-    createCmdFile && fs.chmod(`${to}.cmd`, 0o755)
-  ])
+function chmodShim (to) {
+  return fs.chmod(to, 0o755)
 }
 
 /**
