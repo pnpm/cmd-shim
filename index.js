c26e5d2e1e51d047d6c0ed9494406953ba89b8bd
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-03-18 15:05:54 +0900

    fix: correct wrong type in JSDoc of `generateCmdShim`
    Co-Authored-By: tats-u <tats.u@live.jp>

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ function writeShim (src, to, srcRuntimeInfo, generateShimScript, opts) {
  * @param {string} src Path to the executable or script.
  * @param {string} to Path to the shim to be created.
  * It is highly recommended to end with `.cmd` (or `.bat`).
- * @param {opts} opts Options.  It must contain `.prog` and `.args`.
+ * @param {Options} opts Options.
  * @return {string} The content of shim.
  */
 function generateCmdShim (src, to, opts) {
